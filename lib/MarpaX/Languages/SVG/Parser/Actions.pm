699cc5238f63ea7eb40c095500c324b33a7d8532
ronsavage <ron@savage.net.au>
2014-09-17 17:29:54 +1000

    Update Build.PL and Makefile.PL re module being on github

--- lib/MarpaX/Languages/SVG/Parser/Actions.pm
+++ lib/MarpaX/Languages/SVG/Parser/Actions.pm
@@ -10,7 +10,7 @@ my($param_count);
 
 our $logger;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 # ------------------------------------------------
 
--- lib/MarpaX/Languages/SVG/Parser/SAXHandler.pm
+++ lib/MarpaX/Languages/SVG/Parser/SAXHandler.pm
@@ -75,7 +75,7 @@ has text_stack =>
 	required => 0,
 );
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 # -----------------------------------------------
 
